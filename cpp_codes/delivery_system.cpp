1464d23d4e1afa41cd79a54c2af01aa40131192b
kendyow <carkenvin@gmail,com>
2026-04-03 22:35:32 +0800

    Added some functions that will be used

--- cpp_codes/delivery_system.cpp
+++ cpp_codes/delivery_system.cpp
@@ -6,11 +6,30 @@ class FoodDeliverySystem
 {
 private:
 public:
+    // Place order
+    void placeOrder()
+
+        // Process order
+        void processOrder()
+
+        // Undo last order
+        void Undo()
+
+        // Display (iterative)
+        void DisplayOrder()
+
+        // Recursive Display (normal queue)
+        void recursiveDisplay()
+
+            void showRecursive()
+
+        // Search order
+        void searchOrder()
+
 }
 
 int
 main()
-
 {
 
     cout << "==========MENU==========";
