b1ee96acbbb514dcff5b91ca35aa7cd0a5207d8a
kendyow <carkenvin@gmail,com>
2026-04-13 21:26:42 +0800

    Added the struct and their elements

--- cpp_codes/delivery_system.cpp
+++ cpp_codes/delivery_system.cpp
@@ -2,30 +2,11 @@
 #include <iomanip>
 using namespace std;
 
-class FoodDeliverySystem
+struct Order
 {
-private:
-public:
-    // Place order
-    void placeOrder()
-
-        // Process order
-        void processOrder()
-
-        // Undo last order
-        void Undo()
-
-        // Display (iterative)
-        void DisplayOrder()
-
-        // Recursive Display (normal queue)
-        void recursiveDisplay()
-
-            void showRecursive()
-
-        // Search order
-        void searchOrder()
-
+    int id;
+    string name;
+    int hr;
 }
 
 int
